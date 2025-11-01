bf94ebff9fdbf84863f8b062a072d65430fdb947
Poojan Desai <poojandesai06@gmail.com>
2025-11-01 15:00:21 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,22 @@
+let box = document.querySelector("#inp-check");
+let bar = document.querySelector(".fa-solid");
+let rightnav = document.querySelector(".nav-right");
+
+
+function work() {
+  rightnav.style.top = "100px";
+  rightnav.style.left = "30px";
+}
+
+function reversework() {
+  rightnav.style.top = "-1000px";
+  rightnav.style.left = "-1000px";
+}
+
+bar.addEventListener("click", () => {
+  if (box.checked == false) {
+    work();
+  } else if (box.checked == true) {
+    reversework();
+  }
+});
